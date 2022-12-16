984eace8d8943103b49d6bddc303ae5c927002f7
yeojyann <120380178+yeojyann@users.noreply.github.com>
2022-12-16 11:26:20 +0800

    Update MyCsharp.cs
    @sti-admin

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -7,11 +7,12 @@ public class MyCsharp
 		// Insert code from line below this comment where it instantiates the MyCsharp object into variable named pin, then call the method named printName(String name);
     // Expected Line 1, replace this comment with code on pull request
     // Expected Line 2, replace this comment with code on pull request
+		MyCsharp pin + new MyCsharp();
+		pin.printName("Yeoj Yann Shane M. Nunez")
 	}
 	
-	public void printName(string Yeoj)
+	public void printName(string name)
 	{
-		Console.WriteLine("Hello! My name is " + Yeoj);
-		Console.WriteLine("I am + 21 years old")
+		Console.WriteLine("Hello! My name is " + name);	
 	}
 }
